2c70b3da379a06b8f227f83a8ab02587a2f577c6
rgomet724 <gometromain71@gmail.com>
2026-07-20 14:47:47 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,29 +1,7 @@
-const CACHE_NAME='phenix-v55';
-self.addEventListener('install',event=>{self.skipWaiting();});
-self.addEventListener('activate',event=>{
-  event.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
-});
-self.addEventListener('fetch',event=>{
-  const req=event.request;
-  if(req.method!=='GET') return;
-  if(req.mode==='navigate'){
-    event.respondWith(
-      fetch(req).then(res=>{
-        const copy=res.clone();
-        caches.open(CACHE_NAME).then(c=>c.put(req,copy));
-        return res;
-      }).catch(()=>caches.match(req).then(r=>r||caches.match('/')))
-    );
-    return;
-  }
-  event.respondWith(
-    fetch(req).then(res=>{
-      const copy=res.clone();
-      caches.open(CACHE_NAME).then(c=>c.put(req,copy));
-      return res;
-    }).catch(()=>caches.match(req))
-  );
-});
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>event.waitUntil((async()=>{
+  const names=await caches.keys();
+  await Promise.all(names.map(name=>caches.delete(name)));
+  await self.clients.claim();
+})()));
+self.addEventListener('fetch',()=>{});
