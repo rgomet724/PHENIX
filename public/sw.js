f997a422b327624d4bd6c3435994c489407d2ac0
rgomet724 <gometromain71@gmail.com>
2026-07-20 14:09:41 +0200

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='phenix-v52';
+const CACHE_NAME='phenix-v53';
 self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{
   event.waitUntil(
